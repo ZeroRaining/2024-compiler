8962a2614858eaaff8452ab28b4cf97552a7aa0a
焦子谦 <22371047@buaa.edu.cn>
2024-06-30 16:17:30 +0800

    基本块和指令分别继承各自的链表节点类

--- src/frontend/ir/instr/Instruction.java
+++ src/frontend/ir/instr/Instruction.java
@@ -1,7 +1,8 @@
 package frontend.ir.instr;
 
+import Utils.CustomList;
 import frontend.ir.Value;
 
-public abstract class Instruction implements Value {
+public abstract class Instruction extends CustomList.Node<Instruction> implements Value {
     public abstract String print();
 }
