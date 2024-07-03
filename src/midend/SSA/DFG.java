cb8545e1aafdaa9f6950fed2ca77a2ece408a8c8
郭润林 <22373095@buaa.edu.cn>
2024-07-03 15:59:52 +0800

    restore

--- src/midend/SSA/DFG.java
+++ src/midend/SSA/DFG.java
@@ -1,18 +1,38 @@
 package midend.SSA;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
+import Utils.CustomList;
+import frontend.ir.instr.Instruction;
+import frontend.ir.instr.terminator.JumpInstr;
+import frontend.ir.structure.BasicBlock;
+import frontend.ir.structure.Function;
+
+import java.util.*;
 
 public class DFG {
     private Queue<Integer> worklist;
     private HashMap<Integer, Boolean> visited;
     private HashMap<Integer, Boolean> placed;
-
-    public DFG() {
+    private ArrayList<Function> functions;
+    public DFG(ArrayList<Function> functions) {
         worklist = new LinkedList<Integer>();
         visited = new HashMap<>();
         placed = new HashMap<>();
+        this.functions = functions;
+        for (Function function : functions) {
+            removeBlk(function);
+        }
     }
+
+    private void removeBlk(Function function) {
+        Iterator<CustomList.Node> blks = function.getBasicBlocks().iterator();
+        while (blks.hasNext()) {
+            BasicBlock block = (BasicBlock) blks.next();
+            Instruction instr = (Instruction) block.getInstructions().getTail();
+            if (instr instanceof JumpInstr) {
+
+            }
+        }
+    }
+
 
 }
