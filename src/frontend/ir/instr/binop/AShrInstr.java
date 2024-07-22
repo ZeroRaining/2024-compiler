394f4c12617d6b53aebbb04a6cd4b034d2dee3f5
焦子谦 <22371047@buaa.edu.cn>
2024-07-22 10:00:57 +0800

    给左右移位操作准备一下简化操作，万一有用呢

--- src/frontend/ir/instr/binop/AShrInstr.java
+++ src/frontend/ir/instr/binop/AShrInstr.java
@@ -2,6 +2,7 @@ package frontend.ir.instr.binop;
 
 import frontend.ir.DataType;
 import frontend.ir.Value;
+import frontend.ir.constvalue.ConstInt;
 
 public class AShrInstr extends BinaryOperation {
     public AShrInstr(int result, Value op1, Value op2) {
@@ -12,6 +13,9 @@ public class AShrInstr extends BinaryOperation {
     
     @Override
     public Value operationSimplify() {
+        if (op1 instanceof ConstInt && op2 instanceof ConstInt) {
+            return new ConstInt(((ConstInt) op1).getNumber() >> ((ConstInt) op2).getNumber());
+        }
         return null;
     }
 }
