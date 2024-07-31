da5661fc4728bb50cc2689737db6e96364d78f85
郭润林 <22373095@buaa.edu.cn>
2024-07-31 15:50:27 +0800

    add loopanalysis

---
+++ src/midend/loop/Loop.java
@@ -0,0 +1,77 @@
+package midend.loop;
+
+import frontend.ir.Value;
+import frontend.ir.instr.otherop.cmp.CmpCond;
+import frontend.ir.structure.BasicBlock;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Loop {
+    private BasicBlock entering;
+    private BasicBlock header;
+    private BasicBlock exit;
+    private HashSet<BasicBlock> exiting = new HashSet<>();
+    private HashSet<BasicBlock> latch = new HashSet<>();
+    private HashSet<BasicBlock> blks = new HashSet<>();
+    private ArrayList<Loop> inner = new ArrayList<>();
+    private Loop prtLoop;
+
+    private Value var;
+    private Value begin;
+    private Value end;
+    private Value step;
+    private CmpCond cmpCond;
+
+    public Loop(BasicBlock header) {
+        this.header = header;
+        this.addBlk(header);
+        prtLoop = null;
+    }
+
+
+
+    public BasicBlock getHeader() {
+        return header;
+    }
+
+    public Loop getPrtLoop() {
+        return prtLoop;
+    }
+
+    public void setPrtLoop(Loop prtLoop) {
+        this.prtLoop = prtLoop;
+    }
+
+    public void addLoop(Loop subLoop) {
+        inner.add(subLoop);
+    }
+
+    public void addBlk(BasicBlock blk) {
+        blks.add(blk);
+    }
+
+    public void addExitingBlk(BasicBlock blk) {
+        exiting.add(blk);
+    }
+
+    public void addLatchBlk(BasicBlock blk) {
+        latch.add(blk);
+    }
+
+    public void setExit(BasicBlock blk) {
+        exit = blk;
+    }
+
+    public BasicBlock getExit() {
+        return exit;
+    }
+
+    public ArrayList<Loop> getInner() {
+        return inner;
+    }
+
+    public HashSet<BasicBlock> getBlks() {
+        return blks;
+    }
+}
