adf4d457c87dc6042b7967daa571ab9f89237d6a
郭润林 <22373095@buaa.edu.cn>
2024-08-02 16:18:33 +0800

    LoopInvariantCodeMotion 10.0: fix bug in remove from list with use remain

--- src/Compiler.java
+++ src/Compiler.java
@@ -65,16 +65,18 @@ public class Compiler {
             MergeBlock.execute(functions, false);
             DeadBlockRemove.execute(functions);
             RemoveUseLessPhi.execute(functions);
-
+//            BufferedWriter irWriter = new BufferedWriter(new FileWriter("loopBefore"));
+//            program.printIR(irWriter);
+//            irWriter.close();
             //循环分析
             DFG.execute(functions);
             AnalysisLoop.execute(functions);
 //            LCSSA.execute(functions);
             LoopInvariantMotion.execute(functions);
 
-            BufferedWriter irWriter = new BufferedWriter(new FileWriter("gvnBefore"));
-            program.printIR(irWriter);
-            irWriter.close();
+//            irWriter = new BufferedWriter(new FileWriter("gvnBefore"));
+//            program.printIR(irWriter);
+//            irWriter.close();
             //second
             DFG.execute(functions);
             DeadCodeRemove.execute(functions);
@@ -85,7 +87,7 @@ public class Compiler {
             DeadBlockRemove.execute(functions);
             RemoveUseLessPhi.execute(functions);
         }
-//        Function.blkLabelReorder();
+        Function.blkLabelReorder();
 
         if (arg.toTime()) {
             optimizeEndTime = System.currentTimeMillis();
