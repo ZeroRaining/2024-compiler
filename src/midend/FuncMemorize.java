98412caed405a22c512020f214985b0a2978c7be
焦子谦 <22371047@buaa.edu.cn>
2024-08-07 16:07:36 +0800

    构建直接返回块

--- src/midend/FuncMemorize.java
+++ src/midend/FuncMemorize.java
@@ -9,6 +9,13 @@ import frontend.ir.instr.binop.MulInstr;
 import frontend.ir.instr.binop.SRemInstr;
 import frontend.ir.instr.convop.Fp2Si;
 import frontend.ir.instr.memop.GEPInstr;
+import frontend.ir.instr.memop.LoadInstr;
+import frontend.ir.instr.memop.StoreInstr;
+import frontend.ir.instr.otherop.cmp.Cmp;
+import frontend.ir.instr.otherop.cmp.CmpCond;
+import frontend.ir.instr.otherop.cmp.ICmpInstr;
+import frontend.ir.instr.terminator.BranchInstr;
+import frontend.ir.instr.terminator.JumpInstr;
 import frontend.ir.structure.BasicBlock;
 import frontend.ir.structure.FParam;
 import frontend.ir.structure.Function;
@@ -85,11 +92,25 @@ public class FuncMemorize {
                 ArrayList<Value> indexList = new ArrayList<>();
                 indexList.add(hashVal);
                 GEPInstr dataPtr = new GEPInstr(function.getAndAddRegIndex(), new ArrayList<>(indexList), globalData);
-                GEPInstr usedPtr = new GEPInstr(function.getAndAddRegIndex(), new ArrayList<>(indexList), globalData);
+                GEPInstr usedPtr = new GEPInstr(function.getAndAddRegIndex(), new ArrayList<>(indexList), globalUsed);
                 bbHash.addInstruction(dataPtr);
                 bbHash.addInstruction(usedPtr);
                 
+                // 检查一下之前有没有算过这个值，如果算过进入准备返回，否则正常开始计算
+                LoadInstr usedVal = new LoadInstr(function.getAndAddRegIndex(), globalUsed, usedPtr);
+                bbHash.addInstruction(usedVal);
+                Cmp usedCond = new ICmpInstr(function.getAndAddRegIndex(), CmpCond.EQ, usedVal, ConstInt.Zero);
+                bbHash.addInstruction(usedCond);
+                BranchInstr branch = new BranchInstr(usedCond, (BasicBlock) function.getBasicBlocks().getHead(), bbRet);
+                bbHash.addInstruction(branch);
                 
+                // 构建直接返回块：取出对应的记录结果并返回
+                LoadInstr dataVal = new LoadInstr(function.getAndAddRegIndex(), globalData, dataPtr);
+                bbRet.addInstruction(dataVal);
+                StoreInstr storeRetData = new StoreInstr(dataVal, function.getFuncSymTab().getSym(""));
+                bbRet.addInstruction(storeRetData);
+                JumpInstr jump = new JumpInstr((BasicBlock) function.getBasicBlocks().getTail());
+                bbRet.addInstruction(jump);
             }
         }
     }
